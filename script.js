e855bee1a40f5efb0a4221ac9e2fc158c1b4b832
Koan Loc <loc.koan@yahoo.com>
2020-05-18 23:22:40 -0500

    submission

--- script.js
+++ script.js
@@ -1,33 +1,35 @@
 $(document).ready(function () {
 
     var cityName = ""
-    var queryURL1 = "api.openweathermap.org/data/2.5/weather?q=";
-    var queryURL5 = "api.openweathermap.org/data/2.5/forecast?q=";
+    var queryURL1 = "https://api.openweathermap.org/data/2.5/weather?q=";
+    var queryURL5 = "https://api.openweathermap.org/data/2.5/forecast?q=";
+    var queryUVI = "https://api.openweathermap.org/data/2.5/uvi?";
     var apiKey = "&appid=228a5030262a91d0915376addc531695";
     var unitImperial = "&units=imperial";
     
-    // example = http://api.openweathermap.org/data/2.5/weather?q=houston&appid=228a5030262a91d0915376addc531695&units=imperial
-    // example = http://api.openweathermap.org/data/2.5/forecast?q=houston&appid=228a5030262a91d0915376addc531695&appid=228a5030262a91d0915376addc531695
-    // example = http://api.openweathermap.org/data/2.5/uvi?appid=228a5030262a91d0915376addc531695&lat=29.76&lon=-95.36
+    // example = https://api.openweathermap.org/data/2.5/weather?q=houston&appid=228a5030262a91d0915376addc531695&units=imperial
+    // example = https://api.openweathermap.org/data/2.5/forecast?q=houston&appid=228a5030262a91d0915376addc531695&appid=228a5030262a91d0915376addc531695
+    // example = https://api.openweathermap.org/data/2.5/uvi?appid=228a5030262a91d0915376addc531695&lat=29.76&lon=-95.36
     
-    var fullURL1 = queryURL1 + cityName + apiKey + unitImperial;
-    var fullURL5 = queryURL5 + cityName + apiKey + unitImperial;
+    // var fullURL1 = queryURL1 + cityName + apiKey + unitImperial;
+    // var fullURL5 = queryURL5 + cityName + apiKey + unitImperial;
+    
     //$("#currentCity").empty();
     
     // dates to be inserted
     var currentDate = moment().format("M/DD/YYYY");
-    var forecastCard1 = moment().add(1,"days").format("M/DD/YYYY")
-    var forecastCard2 = moment().add(1,"days").format("M/DD/YYYY")
-    var forecastCard3 = moment().add(1,"days").format("M/DD/YYYY")
-    var forecastCard4 = moment().add(1,"days").format("M/DD/YYYY")
-    var forecastCard5 = moment().add(1,"days").format("M/DD/YYYY")
+    var forecastCard1 = moment().add(1,"days").format("M/DD/YYYY");
+    var forecastCard2 = moment().add(2,"days").format("M/DD/YYYY");
+    var forecastCard3 = moment().add(3,"days").format("M/DD/YYYY");
+    var forecastCard4 = moment().add(4,"days").format("M/DD/YYYY");
+    var forecastCard5 = moment().add(5,"days").format("M/DD/YYYY");
 
     // button 
     $("#cityButton").on("click", function(event) {
         
         event.preventDefault();
 
-        var cityName = $("#cityInput").val();
+        cityName = $("#cityInput").val();
         var PreviousCitySearches = [];
         
         PreviousCitySearches = JSON.parse(localStorage.getItem("PreviousCitySearches")) || [];
@@ -37,21 +39,21 @@ $(document).ready(function () {
         runWeatherApp(cityName);
     });
     
-    // $("#inputCityPast").on("click", function(event) {
-    //     event.preventDefault();
-    //     var cityName = ($(this).text());
-    //     runWeatherApp (cityName);
-    // });
+    $("#inputCityPast").on("click", function(event) {
+        event.preventDefault();
+        var cityName = ($(this).text());
+        runWeatherApp (cityName);
+    });
 
     function runWeatherApp (cityName) {
 
         //clean out previous data in html
         $("#currentCity").empty();
-        $("#forcastDay1").empty();
-        $("#forcastDay2").empty();
-        $("#forcastDay3").empty();
-        $("#forcastDay4").empty();
-        $("#forcastDay5").empty();
+        $("#forecastDay1").empty();
+        $("#forecastDay2").empty();
+        $("#forecastDay3").empty();
+        $("#forecastDay4").empty();
+        $("#forecastDay5").empty();
 
         var fullURL1 = queryURL1 + cityName + apiKey + unitImperial;
 
@@ -66,7 +68,26 @@ $(document).ready(function () {
             $("#currentWindSpeed").text("Wind Speed: " + response.wind.speed + " MPH");
             //$("#currentUvIndex").text("UV Index: " + response);
             // UV index not found in above API/string, is it from another API?
+            
         });
+
+        /* UV section, need to figure how to tie in with above
+
+        var longitude = response.coord.lon;
+        var latitude = response.coord.lat;
+        var fullUVI = queryUVI + apiKey + "&lat=" + latitude + "&lon=" + longitude;
+
+        if (response.value <= 2) {
+            $("#currentUvIndex").css("background-color", "green");
+        } else if (response.value <= 5) {
+            $("#currentUvIndex").css("background-color", "yellow");
+        } else if (response.value <= 7) {
+            $("#currentUvIndex").css("background-color", "orange");
+        } else if (response.value <= 10) {
+            $("#currentUvIndex").css("background-color", "red");
+        } else if (response.value <= 11) {
+            $("#currentUvIndex").css("background-color", "purple");
+        }; */
 
         // forecast cards
         var fullURL5 = queryURL5 + cityName + apiKey + unitImperial;
@@ -76,27 +97,34 @@ $(document).ready(function () {
             method: "GET"
         }).then(function (response) {
             console.log(fullURL5);
+
+            // another card 1
+            // $("#forecastDay1").append(
+            //     "<div class='card-body'>" + forecastCard1 + "</div>" + 
+            //     "<div class='card-header>" + "Temp: " + response.daily[1].temp.day + " °F" + "</div>" + 
+            //     "<div class='card-text>" + "Humidity: " + response.daily[1].humidity + " %" + "</div>"
+            // );
+
             // card 1
             $("#forecastDay1").text(forecastCard1);
             $("#forecastDay1").text("Temp: " + response.daily[0].temp.day + " °F");
             $("#forecastDay1").text("Humidity: " + response.daily[0].humidity + " %");
-            // card 2
-            $("#forecastDay1").text(forecastCard2);
-            $("#forecastDay1").text("Temp: " + response.daily[1].temp.day + " °F");
-            $("#forecastDay1").text("Humidity: " + response.daily[1].humidity + " %");
+            //card 2
+            $("#forecastDay2").text(forecastCard2);
+            $("#forecastDay2").text("Temp: " + response.daily[1].temp.day + " °F");
+            $("#forecastDay2").text("Humidity: " + response.daily[1].humidity + " %");
             // card 3
-            $("#forecastDay1").text(forecastCard3);
-            $("#forecastDay1").text("Temp: " + response.daily[2].temp.day + " °F");
-            $("#forecastDay1").text("Humidity: " + response.daily[2].humidity + " %");
+            $("#forecastDay3").text(forecastCard3);
+            $("#forecastDay3").text("Temp: " + response.daily[2].temp.day + " °F");
+            $("#forecastDay3").text("Humidity: " + response.daily[2].humidity + " %");
             // card 4
-            $("#forecastDay1").text(forecastCard4);
-            $("#forecastDay1").text("Temp: " + response.daily[3].temp.day + " °F");
-            $("#forecastDay1").text("Humidity: " + response.daily[3].humidity + " %");
+            $("#forecastDay4").text(forecastCard4);
+            $("#forecastDay4").text("Temp: " + response.daily[3].temp.day + " °F");
+            $("#forecastDay4").text("Humidity: " + response.daily[3].humidity + " %");
             // card 5
-            $("#forecastDay1").text(forecastCard5);
-            $("#forecastDay1").text("Temp: " + response.daily[4].temp.day + " °F");
-            $("#forecastDay1").text("Humidity: " + response.daily[4].humidity + " %");
-
+            $("#forecastDay5").text(forecastCard5);
+            $("#forecastDay5").text("Temp: " + response.daily[4].temp.day + " °F");
+            $("#forecastDay5").text("Humidity: " + response.daily[4].humidity + " %");
         });
     };
 
